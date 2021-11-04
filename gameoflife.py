b24eec3b1353502cccb2030faaba196c877ea3d5
Lepiz-Loca <46353769+Lepiz-Loca@users.noreply.github.com>
2021-11-04 21:51:23 +0100

    Update gameoflife.py

--- gameoflife.py
+++ gameoflife.py
@@ -1,21 +1,27 @@
 import numpy as np
 import scipy.ndimage as nd
 
-DIM = 5, 5
-KERNEL = np.array([[0, 1, 0],
+DIM = 5, 5 #Matrix dimensions
+KERNEL = np.array([[1, 1, 1],
                    [1, 0, 1],
-                   [0, 1, 0]], dtype=np.uint8)
+                   [1, 1, 1]], dtype=np.uint8) #"neighbour" detection
 
-universe = np.random.randint(2, size=DIM, dtype=np.uint8)
+universe = np.random.randint(2, size=DIM, dtype=np.uint8) 
+#"Counting" the neighbours 
 neighbor_count = nd.convolve(universe, KERNEL, mode="constant")
 new_state = np.empty(DIM)
 np.copyto(new_state, neighbor_count, casting='same_kind')
 
+#Applying the Game of Life rules
 new_state = np.where(neighbor_count < 2, 0, neighbor_count)
 new_state = np.where(neighbor_count > 3, 0, new_state)
 new_state = np.where(neighbor_count == 3, 1, new_state)
 new_state = np.where(neighbor_count == 2, 1, new_state)
 
 print(universe)
-print(neighbor_count)
+print(neighbor_count) #used for debugging
 print(new_state)
+
+#TODO
+#An actual interface
+#Selecting the initial state of the game instead of it being randomized
